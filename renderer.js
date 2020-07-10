a4b7166d88777167abe37a9cd436fd18c1d4c81d
wangpingping <wangpingping@pxjy.com>
2020-07-10 21:02:52 +0800

    init

---
+++ renderer.js
@@ -0,0 +1,17 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+const webview = document.getElementById("foo");
+webview.addEventListener("new-window", (e) => {
+  const protocol = require("url").parse(e.url).protocol;
+  if (protocol === "http:" || protocol === "https:") {
+    webview.src = e.url;
+  }
+});
+
+webview.addEventListener('dom-ready', () => {
+  webview.openDevTools()
+})
